7490543f4a26fdf79e836ec033ab5151235c1099
hugomflavio <40389021+hugomflavio@users.noreply.github.com>
2019-10-15 16:45:47 +0200

    update createWorkspace

--- R/z_examples.R
+++ R/z_examples.R
@@ -4,14 +4,15 @@
 #'
 #' @export
 #' 
-createWorkspace <- function() {
+createWorkspace <- function(dir = "actel_workspace") {
+  if (!dir.exists(dir)) 
+    dir.create(dir)
   spatial <- data.frame(
     Station.Name = c("Example station1", "Example station2", "Example station3", "Example release1", "Example release2"),
     Latitude = c(8.411, 8.521, 8.402, 8.442, 8.442),
     Longitude = c(40.411, 40.521, 40.402, 40.442, 40.442), 
     Array = c("River1", "River1", "River2", "River1", "River2"), 
     Type = c("Hydrophone", "Hydrophone", "Hydrophone", "Release", "Release"))
-  write.csv(spatial, "spatial.csv", row.names = FALSE)
 
   biometrics <- data.frame(
     Release.date = c("2018-02-01 10:05:00", "2018-02-01 10:10:00", "2018-02-01 10:15:00"), 
@@ -21,18 +22,20 @@ createWorkspace <- function() {
     Weight.g = c(40, 60, 50), 
     Array = c("Wild", "Hatchery", "Wild"), 
     Release.site = c("Example release1", "Example release1", "Example release2"))
-  write.csv(biometrics, "biometrics.csv", row.names = FALSE)
 
   deployments <- data.frame(
     Receiver = c("123001", "123002", "331"), 
     Station.Name = c("Example station1", "Example station2", "Example station3"),
     Start = c("2018-01-25 12:00:00", "2018-01-25 12:00:00", "2018-01-25 12:00:00"),
     Stop = c("2018-04-03 12:00:00", "2018-04-03 12:00:00", "2018-04-03 12:00:00"))
-  write.csv(deployments, "deployments.csv", row.names = FALSE)
 
-  if (!dir.exists("detections")) 
-    dir.create("detections")
-  message("M: Workspace files created.")
+  write.csv(spatial, paste(dir, "spatial.csv", sep ="/"), row.names = FALSE)
+  write.csv(biometrics, paste(dir, "biometrics.csv", sep ="/"), row.names = FALSE)
+  write.csv(deployments, paste(dir, "deployments.csv", sep ="/"), row.names = FALSE)
+
+  if (!dir.exists(paste(dir, "detections", sep ="/"))) 
+    dir.create(paste(dir, "detections", sep ="/"))
+  message(paste0("M: Workspace files created in folder '", dir,"'."))
 }
 
 #' Create an example Workspace
