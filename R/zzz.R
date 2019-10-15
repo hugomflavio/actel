1da1632c10ad507e40f6e157bf412613ee6aa799
hugomflavio <40389021+hugomflavio@users.noreply.github.com>
2019-10-15 16:45:58 +0200

    Fix version check function

--- R/zzz.R
+++ R/zzz.R
@@ -7,13 +7,15 @@
   }
   validIP <- "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[.]){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
 	if (any(grep(validIP, ipmessage))) {
-	  rep.ver <- unlist(strsplit(readLines('https://raw.githubusercontent.com/hugomflavio/actel/master/DESCRIPTION')[3], " "))[2]
-	  rep.ver.short <- substr(rep.ver, start = 1, stop = nchar(rep.ver) - 5)
-	  rep.ver.num <- as.numeric(gsub(".", "", rep.ver.short))
-	  inst.ver <- utils::packageVersion("actel")
-	  inst.ver.short <- substr(inst.ver, start = 1, stop = nchar(as.character(inst.ver)) - 5) 
-	  inst.ver.num <- as.numeric(gsub(".", "", inst.ver.short))
-	  if (rep.ver.short > inst.ver.short)
-	    packageStartupMessage(paste0("-------------------------------------------------------------\n!!! A NEW VERSION of actel is available! (v.", inst.ver.short, " -> v.", rep.ver.short, ")\n!!! You should update actel before continuing.\n!!! To learn how to update actel, run updateActel()\n-------------------------------------------------------------\n"))
+	  rep.ver <- tryCatch(unlist(strsplit(readLines('https://raw.githubusercontent.com/hugomflavio/actel/master/DESCRIPTION')[3], " "))[2], error = function(e) NULL, warning = function(w) NULL)
+	  if (!is.null(rep.ver)) {
+	  	rep.ver.short <- substr(rep.ver, start = 1, stop = nchar(rep.ver) - 5)
+		  rep.ver.num <- unlist(strsplit(rep.ver.short, ".", fixed = TRUE))
+		  inst.ver <- utils::packageVersion("actel")
+		  inst.ver.short <- substr(inst.ver, start = 1, stop = nchar(as.character(inst.ver)) - 5) 
+		  inst.ver.num <- unlist(strsplit(inst.ver.short, ".", fixed = TRUE))
+		  if (any(rep.ver.short > inst.ver.short))
+		    packageStartupMessage(paste0("-------------------------------------------------------------\n!!! A NEW VERSION of actel is available! (v.", inst.ver.short, " -> v.", rep.ver.short, ")\n!!! You should update actel before continuing.\n!!! To learn how to update actel, run updateActel()\n-------------------------------------------------------------\n"))
+		}
 	}
 }
