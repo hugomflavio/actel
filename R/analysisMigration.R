ecb2cea967f805080dedd4e6e082220a5df0ea52
hugomflavio <40389021+hugomflavio@users.noreply.github.com>
2019-10-18 17:20:21 +0200

    feed arrays to assembleTimetable

--- R/analysisMigration.R
+++ R/analysisMigration.R
@@ -173,6 +173,7 @@ migration <- function(path = NULL, sections, success.arrays = NULL, minimum.dete
   }
   
   recipient <- assembleTimetable(movements = movements, sections = sections, spatial = spatial, 
+  recipient <- assembleTimetable(movements = movements, sections = sections, spatial = spatial, arrays = arrays,
     minimum.detections = minimum.detections, dist.mat = dist.mat, invalid.dist = invalid.dist, 
     speed.method = speed.method, if.last.skip.section = if.last.skip.section, success.arrays = success.arrays, 
     override = override, cautious.assignment = cautious.assignment)
