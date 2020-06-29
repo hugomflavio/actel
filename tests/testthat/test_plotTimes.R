ebd1c7afb62137ec08f06054348d36e2e58493e3
hugomflavio <40389021+hugomflavio@users.noreply.github.com>
2020-06-29 18:16:07 +0200

    expand plotTimes tests

--- tests/testthat/test_plotTimes.R
+++ tests/testthat/test_plotTimes.R
@@ -48,15 +48,17 @@ test_that("plotTimes fail-safes kick in when needed", {
 
 	expect_error(plotTimes(times = times, col = "blue"),
 		"'col' must be of the same length as 'times' (1 != 2).", fixed = TRUE)
+})
 
+test_that("plotTimes can handle lots of datasets", {
 	xtimes <- list(a = times[[1]], b = times[[1]], c = times[[1]], d = times[[1]], e = times[[1]],
-		f = times[[1]], g = times[[1]], h = times[[1]], i = times[[1]])
+		f = times[[1]], g = times[[1]], h = times[[1]], i = times[[1]], j = times[[1]])
 
-	tryCatch(plotTimes(times = xtimes), warning = function(w) stop("plotTimes threw an unexpected warning"))
+	tryCatch(plotTimes(times = xtimes), warning = function(w) stop("plotTimes threw an unexpected warning"))	
 })
 
 test_that("plotTimes returns no errors on actual data, plus saves files", {
-	tryCatch(plotTimes(times = times, night = c("20:00", "06:00"), title = "This is a test!"),
+	tryCatch(plotTimes(times = times, night = c("20:00", "06:00"), col = c("blue", "red"), title = "This is a test!"),
 		warning = function(w) stop("A warning was issued where it should not have been."))
 
 	tryCatch(plotTimes(times = times, night = c("20:00", "06:00"), title = "This is a test!"),
@@ -71,6 +73,45 @@ test_that("plotTimes returns no errors on actual data, plus saves files", {
 
 	expect_true(file.exists("test_plotTimes_output.svg"))
 	file.remove("test_plotTimes_output.svg")
+
+	expect_warning(plotTimes(times = times, file = "test_plotTimes_output.svg", cex = 2),
+		"When saving vectorial plots, it is recommended to refine the 'width' and 'height', rather than the 'cex'.", fixed = TRUE)
+})
+
+test_that("different extensions are working in plotTimes", {
+	tryCatch(plotTimes(times = times, file = "test.svg"), 
+		warning = function(w) stop("plotTimes threw an unexpected warning"))	
+	expect_true(file.exists("test.svg"))
+	file.remove("test.svg")
+
+	tryCatch(plotTimes(times = times, file = "test.pdf"), 
+		warning = function(w) stop("plotTimes threw an unexpected warning"))	
+	expect_true(file.exists("test.pdf"))
+	file.remove("test.pdf")
+
+	tryCatch(plotTimes(times = times, file = "test.png"), 
+		warning = function(w) stop("plotTimes threw an unexpected warning"))	
+	expect_true(file.exists("test.png"))
+	file.remove("test.png")
+
+	tryCatch(plotTimes(times = times, file = "test.tiff"), 
+		warning = function(w) stop("plotTimes threw an unexpected warning"))	
+	expect_true(file.exists("test.tiff"))
+	file.remove("test.tiff")
+
+	expect_error(plotTimes(times = times, file = "test"), 
+		"Could not recognise 'file' extension (recognised extensions: .svg, .pdf, .png, .tiff).", fixed = TRUE)	
+})
+
+test_that("plotTimes complains if xjust is set in corner legend", {
+	expect_warning(plotTimes(times = times, xjust = "centre"), 
+		"'xjust' was set but legend is being plotted in the corner. Ignoring 'xjust'.", fixed = TRUE)	
+
+	tryCatch(plotTimes(times = times, legend.pos = "bottom"), 
+		warning = function(w) stop("plotTimes threw an unexpected warning"))	
+
+	tryCatch(plotTimes(times = times, legend.pos = "bottom", xjust = "right"), 
+		warning = function(w) stop("plotTimes threw an unexpected warning"))	
 })
 
 setwd(tests.home)
