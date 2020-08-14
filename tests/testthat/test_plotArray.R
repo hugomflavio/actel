93752cbf8ad796545398dcd2524bec5817280bd9
hugomflavio <40389021+hugomflavio@users.noreply.github.com>
2020-08-14 18:51:02 +0200

    Update test_plotArray.R

--- tests/testthat/test_plotArray.R
+++ tests/testthat/test_plotArray.R
@@ -2,8 +2,8 @@ skip_on_cran()
 
 test_that("plotArray works properly", {
 	tryCatch(plotArray(example.results, arrays = "A1"),
-	warning = function(w) stop("plotMoves threw an unexpected warning!\n", w))
+	warning = function(w) stop("plotArray threw an unexpected warning!\n", w))
 
-	tryCatch(plotMoves(example.results, arrays = c("A1", "A2", cumulative = TRUE),
-	warning = function(w) stop("plotMoves threw an unexpected warning!\n", w))
+	tryCatch(plotMoves(example.results, arrays = c("A1", "A2"), cumulative = TRUE),
+	warning = function(w) stop("plotArray threw an unexpected warning!\n", w))
 })
