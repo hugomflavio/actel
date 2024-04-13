hugomflavio/actel

commit bbffed29e49a37cfe36313fc2afc4150f12d3019

correct dot wildcards on grep and sub calls throughout

R/load.R
@@ -854,7 +854,7 @@ loadSpatial <- function(input = "spatial.csv", section.order = NULL){
   if (!is.na(link <- match("Station.Name", colnames(input))))
     colnames(input)[link] <- "Station.name"
   # Check missing Station.name
-  if (!any(grepl("^Station.name$", colnames(input)))) {
+  if (!any(grepl("^Station\\.name$", colnames(input)))) {
     stopAndReport("The spatial input must contain a 'Station.name' column.")
   } else {
     # Check all station names are unique
@@ -1026,7 +1026,7 @@ loadBio <- function(input, tz){
     stopAndReport("The following columns are duplicated in the biometrics: '",
       paste(unique(colnames(bio)[link]), sep = "', '"), "'.")
 
-  if (!any(grepl("^Release.date$", colnames(bio)))) {
+  if (!any(grepl("^Release\\.date$", colnames(bio)))) {
     stopAndReport("The biometrics must contain an 'Release.date' column.")
   }
 
@@ -1057,7 +1057,7 @@ loadBio <- function(input, tz){
     stopAndReport("Some animals have no 'Signal' information. Please double-check the biometrics.")
   }
 
-  if (!any(grepl("^Code.space$", colnames(bio)))) {
+  if (!any(grepl("^Code\\.space$", colnames(bio)))) {
     appendTo(c("Screen", "Report"), "M: No Code.space column was found in the biometrics. Assigning code spaces based on detections.")
   } else {
       if (any(is.na(bio$Code.space)) | any(bio$Code.space == ""))
@@ -1112,7 +1112,7 @@ loadBio <- function(input, tz){
 
   # check sensor names
   if (!expect_integer) { 
-    if (!any(grepl("^Sensor.unit$", colnames(bio)))) {
+    if (!any(grepl("^Sensor\\.unit$", colnames(bio)))) {
       appendTo(c("Screen", "Warning"), "Tags with multiple sensors are listed in the biometrics, but a 'Sensor.unit' column could not be found. Skipping sensor unit assignment.")
     } 
     else {
@@ -1147,7 +1147,7 @@ loadBio <- function(input, tz){
   }
 
   # Release site quality checking/creation
-  if (!any(grepl("^Release.site$", colnames(bio)))) {
+  if (!any(grepl("^Release\\.site$", colnames(bio)))) {
     appendTo("Screen", "M: No Release site has been indicated in the biometrics. Creating a 'Release.site' column to avoid function failure. Filling with 'unspecified'.")
     bio$Release.site <- "unspecified"
   } else {
@@ -1326,7 +1326,7 @@ compileDetections <- function(path = "detections", start.time = NULL, stop.time 
             })
           unknown.file <- FALSE
         }
-        if (unknown.file && all(!is.na(match(c("Transmitter", "Receiver"), colnames(aux)))) & any(grepl("^Date.and.Time", colnames(aux)))) {
+        if (unknown.file && all(!is.na(match(c("Transmitter", "Receiver"), colnames(aux)))) & any(grepl("^Date\\.and\\.Time", colnames(aux)))) {
           appendTo("debug", paste0("File '", i, "' matches a Vemco log."))
           output <- tryCatch(processVemcoFile(input = aux), error = function(e) {
               stopAndReport("Something went wrong when processing file '", i, "'. If you are absolutely sure this file is ok, contact the developer.\nOriginal error:", sub("^Error:", "", e))
@@ -1437,7 +1437,7 @@ processThelmaOldFile <- function(input) {
   appendTo("debug", "Running processThelmaOldFile.")
   input <- as.data.frame(input, stringsAsFactors = FALSE)
   output <- data.table(
-    Timestamp = fasttime::fastPOSIXct(sapply(as.character(input[, grep("^Date.and.Time", colnames(input))]), function(x) gsub("Z", "", gsub("T", " ", x))), tz = "UTC"),
+    Timestamp = fasttime::fastPOSIXct(sapply(as.character(input[, grep("^Date\\.and\\.Time", colnames(input))]), function(x) gsub("Z", "", gsub("T", " ", x))), tz = "UTC"),
     Receiver = input$`TBR Serial Number`,
     CodeSpace = input$CodeType,
     Signal = input$Id,
@@ -1473,7 +1473,7 @@ processThelmaNewFile <- function(input) {
   appendTo("debug", "Running processThelmaNewFile.")
   input <- as.data.frame(input, stringsAsFactors = FALSE)
   output <- data.table(
-    Timestamp = fasttime::fastPOSIXct(sapply(as.character(input[, grep("^Date.and.Time", colnames(input))]), function(x) gsub("Z", "", gsub("T", " ", x))), tz = "UTC"),
+    Timestamp = fasttime::fastPOSIXct(sapply(as.character(input[, grep("^Date\\.and\\.Time", colnames(input))]), function(x) gsub("Z", "", gsub("T", " ", x))), tz = "UTC"),
     Receiver = input$Receiver,
     CodeSpace = sapply(input$Protocol, function(x) unlist(strsplit(x, "-", fixed = TRUE))[1]),
     Signal = input$ID,
@@ -1513,9 +1513,9 @@ processVemcoFile <- function(input) {
   input[, "Signal"] <- unlist(lapply(transmitter_aux, function(x) x[3])) # extract only signal
   input[, "Receiver"] <- sapply(input$Receiver, function(x) tail(unlist(strsplit(x, "-")), 1)) # extract only the serial
 
-  colnames(input)[grep("^Date.and.Time", colnames(input))] <- c("Timestamp")
+  colnames(input)[grep("^Date\\.and\\.Time", colnames(input))] <- c("Timestamp")
   colnames(input) <- gsub(" ", ".", colnames(input))
-  if (any(grepl("^Sensor.Value$", colnames(input)))) {
+  if (any(grepl("^Sensor\\.Value$", colnames(input)))) {
     input <- input[, c("Timestamp", "Receiver", "CodeSpace", "Signal", "Sensor.Value", "Sensor.Unit")]
   } else {
     input <- input[, c("Timestamp", "Receiver", "CodeSpace", "Signal")]
@@ -1663,7 +1663,7 @@ splitDetections <- function(detections, bio, exclude.tags = NULL) {
                          Signal = extractSignals(names(my.list)))
 
   # and this one as an index for the target tags
-  if (any(grepl("^Code.space$", colnames(bio))))
+  if (any(grepl("^Code\\.space$", colnames(bio))))
     bio_aux <- bio[, c("Code.space", "Signal")]
   else
     bio_aux <- data.frame(Code.space = NA,
@@ -1673,7 +1673,7 @@ splitDetections <- function(detections, bio, exclude.tags = NULL) {
   bio_aux$Signal_expanded <- lapply(strsplit(as.character(bio$Signal), "|", fixed = TRUE), as.numeric)
 
   # include sensor units, if relevant
-  if (any(grepl("^Sensor.unit$", colnames(bio))))
+  if (any(grepl("^Sensor\\.unit$", colnames(bio))))
     bio_aux$Sensor.unit_expanded <- strsplit(as.character(bio$Sensor.unit), "|", fixed = TRUE)
   else
     bio_aux$Sensor.unit_expanded <- NA
@@ -1727,7 +1727,7 @@ splitDetections <- function(detections, bio, exclude.tags = NULL) {
       output <- my.list[list_matches]
 
       # Find Sensor.unit column in the biometrics
-      if (any(grepl("^Sensor.unit$", colnames(bio)))) {
+      if (any(grepl("^Sensor\\.unit$", colnames(bio)))) {
         # Replace sensor units...
         for (j in 1:length(output)) {
           sensor_index <- match(extractSignals(names(output)[j]), bio_aux$Signal_expanded[[i]])

DESCRIPTION
@@ -1,6 +1,6 @@
 Package: actel
 Title: Acoustic Telemetry Data Analysis
-Version: 1.3.0.9003
+Version: 1.3.0.9004
 Authors@R: person("Hugo", "Flávio", role = c("aut", "cre"), email = "hflavio@wlu.ca", comment = c(ORCID = "0000-0002-5174-1197"))
 Description: Designed for studies where animals tagged with acoustic tags are expected
     to move through receiver arrays. This package combines the advantages of automatic sorting and checking 

R/CJS.R
@@ -32,7 +32,7 @@ getDualMatrices <- function(replicates, CJS = NULL, spatial, detections.list) {
   for(i in 1:length(replicates)) {
     continue <- TRUE
     if (!is.null(CJS)) {
-      if (any(grepl("^max.efficiency$", names(CJS))))
+      if (any(grepl("^max\\.efficiency$", names(CJS))))
         already.calculated <- !is.na(CJS$max.efficiency[names(replicates)[i]])
       else
         already.calculated <- !is.na(CJS$efficiency[names(replicates)[i]])
@@ -334,7 +334,7 @@ assembleMatrices <- function(spatial, movements, status.df, arrays, paths, dotma
     # If the release sites start in different arrays, trim the matrices as needed
     if (length(unique.release.arrays) > 1) {
       for(i in 1:length(aux)){ # for each matrix, find the corresponding release site.
-        the_release_site <- sapply(spatial$release.sites$Standard.name, function(x) grepl(paste0(".", x, "$"), names(aux)[i])) 
+        the_release_site <- sapply(spatial$release.sites$Standard.name, function(x) grepl(paste0("\\.", x, "$"), names(aux)[i])) 
         if(sum(the_release_site) > 1) # if there is more than one matching release site, stop.
           stop("Multiple release sites match the matrix name. Make sure that the release sites' names are not contained within the animal groups or within themselves.\n")
         # else, find which is the first column to keep. This is tricky for multi-branch sites...

R/check.R
@@ -193,10 +193,6 @@ checkArguments <- function(dp, tz, min.total.detections, min.per.event, max.inte
     stopAndReport("'detections.y.axis' should be one of 'stations' or 'arrays'")
   detections.y.axis <- match.arg(detections.y.axis)
 
-  if (!is.character(timestep))
-    stopAndReport("'timestep' should be one of 'days' or 'hours'")
-  timestep <- match.arg(timestep)
-
   # Check that all the overridden tags are part of the study
   if (!is.null(dp) && !is.null(override)) {
     if (is.numeric(override)) {
@@ -212,6 +208,10 @@ checkArguments <- function(dp, tz, min.total.detections, min.per.event, max.inte
   }
 
   # NON-explore checks
+  if (!is.character(timestep))
+    stopAndReport("'timestep' should be one of 'days' or 'hours'")
+  timestep <- match.arg(timestep)
+
   if (!is.null(section.order) && any(table(section.order) > 1))
     stopAndReport("Some section names are duplicated in the 'section.order' argument. Please include each section only once.")
 

R/helper.R
@@ -570,7 +570,7 @@ recoverLog <- function(file, overwrite = FALSE) {
   if (!is.character(file) | length(file) != 1)
     stop("'file' must be a single character string.", call. = FALSE)
 
-  if (!grepl(".txt$", file))
+  if (!grepl("\\.txt$", file))
     file <- paste0(file, ".txt")
 
   if (file.exists(file) & !overwrite)

R/migration.R
@@ -1238,7 +1238,7 @@ assembleTimetable <- function(secmoves, valid.moves, all.moves, spatial, arrays,
   }
   recipient <- c(recipient, "Very.last.array", "Very.last.time", "Status", "Valid.detections", "Valid.events", "Invalid.detections", "Invalid.events", "Backwards.movements", "Max.cons.back.moves", "P.type")
   if (attributes(dist.mat)$valid && speed.method == "last to last")
-    recipient <- recipient[!grepl("Average.speed.in",recipient)]
+    recipient <- recipient[!grepl("Average\\.speed\\.in", recipient)]
 
   timetable <- matrix(nrow = length(secmoves), ncol = length(recipient))
   timetable <- as.data.frame(timetable)
@@ -1285,14 +1285,14 @@ assembleTimetable <- function(secmoves, valid.moves, all.moves, spatial, arrays,
     recipient <- as.data.frame(combine(recipient), stringsAsFactors = FALSE)
 
     # convert numbers to numeric and replace NAs where relevant
-    the.cols <- which(grepl("Times.entered.", colnames(recipient)))
+    the.cols <- which(grepl("Times\\.entered\\.", colnames(recipient)))
     recipient[, the.cols] <- as.numeric(recipient[, the.cols])
     recipient[, the.cols[which(is.na(recipient[, the.cols]))]] <- 0
 
-    the.cols <- which(grepl("Average.speed.to.", colnames(recipient)))
+    the.cols <- which(grepl("Average\\.speed\\.to\\.", colnames(recipient)))
     recipient[, the.cols] <- as.numeric(recipient[, the.cols])
 
-    the.cols <- which(grepl("Average.speed.in.", colnames(recipient)))
+    the.cols <- which(grepl("Average\\.speed\\.in\\.", colnames(recipient)))
     recipient[, the.cols] <- as.numeric(recipient[, the.cols])
     # --
 

R/plot.R
@@ -840,7 +840,7 @@ plotDetections <- function(input, tag, type, y.axis = c("auto", "stations", "arr
   # grey dashes (for migration only)
   if (input$rsp.info$analysis.type == "migration" & like.migration) {
     status.df <- input$status.df
-    relevant.line <- status.df[which(status.df$Transmitter == tag), (grepl("First.arrived", colnames(status.df)) | grepl("Last.left", colnames(status.df)))]
+    relevant.line <- status.df[which(status.df$Transmitter == tag), (grepl("First\\.arrived", colnames(status.df)) | grepl("Last\\.left", colnames(status.df)))]
   } else {
     like.migration <- FALSE
   }
@@ -1243,7 +1243,7 @@ plotTimes <- function(times, night = NULL, circular.scale = c("area", "linear"),
   }
 
   if (!missing(file)) {
-    if (grepl(".svg$", file) | grepl(".pdf$", file)) {
+    if (grepl("\\.svg$", file) | grepl("\\.pdf$", file)) {
       if (cex != 1)
         message("M: When saving vectorial plots, it is recommended to refine the 'width' and 'height', rather than the 'cex'.")
       if (missing(height))
@@ -1257,7 +1257,7 @@ plotTimes <- function(times, night = NULL, circular.scale = c("area", "linear"),
       file <- gsub("\\\\|/|:|\\*|\\?|\\\"|<|>|\\|", "_", file)
     }
 
-    if (grepl(".png$", file) | grepl(".tiff$", file)) {
+    if (grepl("\\.png$", file) | grepl("\\.tiff$", file)) {
       if (missing(height))
         height <- 500
       if (missing(width))
@@ -1265,19 +1265,19 @@ plotTimes <- function(times, night = NULL, circular.scale = c("area", "linear"),
     }
 
     unk.ext <- TRUE
-    if (unk.ext && grepl(".svg$", file)) {
+    if (unk.ext && grepl("\\.svg$", file)) {
       grDevices::svg(file, height = height, width = width, bg = bg)
       unk.ext <- FALSE
     }
-    if (unk.ext && grepl(".pdf$", file)) {
+    if (unk.ext && grepl("\\.pdf$", file)) {
       grDevices::pdf(file, height = height, width = width, bg = bg)
       unk.ext <- FALSE
     }
-    if (unk.ext && grepl(".png$", file)) {
+    if (unk.ext && grepl("\\.png$", file)) {
       grDevices::png(file, height = height, width = width,  bg = bg)
       unk.ext <- FALSE
     }
-    if (unk.ext && grepl(".tiff$", file)) {
+    if (unk.ext && grepl("\\.tiff$", file)) {
       grDevices::tiff(file, height = height, width = width, bg = bg)
       unk.ext <- FALSE
     }
@@ -1311,9 +1311,9 @@ plotTimes <- function(times, night = NULL, circular.scale = c("area", "linear"),
       if (ncol > 1)
         warning("Plotting the legend in the corner but ncol > 1. This will likely lead to bad results.", immediate. = TRUE, call. = FALSE)
       if (ceiling(length(times)/ ncol) > 2) {
-        if (!missing(file) && (grepl(".png$", file) | grepl(".tiff", file)))
+        if (!missing(file) && (grepl("\\.png$", file) | grepl("\\.tiff", file)))
           ylegend <- -0.97 + (0.06 * (ceiling(length(times) / ncol) - 2))
-        if (missing(file) || (grepl(".svg$", file) | grepl(".pdf", file)))
+        if (missing(file) || (grepl("\\.svg$", file) | grepl("\\.pdf", file)))
           ylegend <- -0.97 + (0.08 * (ceiling(length(times) / ncol) - 2))
       } else {
         ylegend <- -0.97      

R/preload.R
@@ -250,18 +250,18 @@ preloadDetections <- function(input, tz, start.time = NULL, stop.time = NULL) {
   if (!is.character(input$CodeSpace))
     input$CodeSpace <- as.character(input$CodeSpace)
 
-	if (length(the.col <- grep("^[S|s]ensor.[U|u]nit$", colnames(input))) > 0)
+	if (length(the.col <- grep("^[S|s]ensor\\.[U|u]nit$", colnames(input))) > 0)
 		colnames(input)[the.col] <- "Sensor.Unit"
 
-	if (length(the.col <- grep("^[S|s]ensor.[V|v]alue$", colnames(input))) > 0)
+	if (length(the.col <- grep("^[S|s]ensor\\.[V|v]alue$", colnames(input))) > 0)
 		colnames(input)[the.col] <- "Sensor.Value"
 	
-	if (!any(grepl("^Sensor.Value$", colnames(input)))) {
+	if (!any(grepl("^Sensor\\.Value$", colnames(input)))) {
 		appendTo(c("Screen", "Warning", "Report"), "Could not find a 'Sensor.Value' column in the detections. Filling one with NA.")
 		input$Sensor.Value <- NA_real_
 	}
 
-	if (!any(grepl("^Sensor.Unit$", colnames(input)))) {
+	if (!any(grepl("^Sensor\\.Unit$", colnames(input)))) {
 		appendTo(c("Screen", "Warning", "Report"), "Could not find a 'Sensor.Unit' column in the detections. Filling one with NA.")
 		input$Sensor.Unit <- NA_character_
 	}

commit 52f6e0f982fa5ebb955fe26da6e17536c003662d

fix bug introduced by bbffed2

R/load.R
@@ -1472,10 +1472,24 @@ processThelmaOldFile <- function(input) {
 processThelmaNewFile <- function(input) {
   appendTo("debug", "Running processThelmaNewFile.")
   input <- as.data.frame(input, stringsAsFactors = FALSE)
+
+  # leave the dots as wildcards as data table does not replace spaces with dots
+  time_col <- grep("^Date.and.Time", colnames(input))
+  time_vec <- fasttime::fastPOSIXct(
+    sapply(
+      as.character(input[, time_col]),
+      function(x) gsub("Z", "", gsub("T", " ", x))
+    ),
+    tz = "UTC"
+  )
+
+  codespace_vec <- sapply(input$Protocol,
+                          function(x) unlist(strsplit(x, "-", fixed = TRUE))[1])
+  
   output <- data.table(
-    Timestamp = fasttime::fastPOSIXct(sapply(as.character(input[, grep("^Date\\.and\\.Time", colnames(input))]), function(x) gsub("Z", "", gsub("T", " ", x))), tz = "UTC"),
+    Timestamp = time_vec,
     Receiver = input$Receiver,
-    CodeSpace = sapply(input$Protocol, function(x) unlist(strsplit(x, "-", fixed = TRUE))[1]),
+    CodeSpace = codespace_vec,
     Signal = input$ID,
     Sensor.Value = input$Data,
     Sensor.Unit = rep(NA_character_, nrow(input)))